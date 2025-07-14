5a8944fd9967eeeea3949998ab317a74052db271
riyadomf <riyad.omf@gmail.com>
2025-07-15 02:40:26 +0600

    Update code for cdi intro

--- src/main/java/com/jee/jeejsfcditutorialsourcecode/GreeterBean.java
+++ src/main/java/com/jee/jeejsfcditutorialsourcecode/GreeterBean.java
@@ -1,6 +1,7 @@
 package com.jee.jeejsfcditutorialsourcecode;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +10,27 @@ import lombok.Setter;
 @Named
 @RequestScoped
 public class GreeterBean {
-    @Getter @Setter
+    @Getter
+    @Setter
     private String name;
+
+
+
+
+
+
+
+
+
+
+    /**
+    This is Dependency Injection!
+    It tells CDI: "Please find a MessageService and give me an instance."
+    **/
+    @Inject
+    MessageService messageService;
+
+    public String getMessage() {
+        return messageService.getGreeting();
+    }
 }
